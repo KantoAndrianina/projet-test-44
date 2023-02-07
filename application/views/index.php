73171714d044c7b18ed41bb19d7f070bf4f1a541
Colin <colinraminoson@gmail.com>
2023-02-07 15:25:30 +0300

    header-footer-index

--- application/views/index.php
+++ application/views/index.php
@@ -2,7 +2,7 @@
   <?php $this->load->view("page/header");?>
   <!--header-------------->
   <!-- Page Content -->
-    <?php $this->load->view($content);?>
+    <?php $this->load->view("page/content");?>
   <!-- /.container -->
   <!-- Footer -->
     <?php $this->load->view("page/footer");?>
