6dac9e8aa88bf3e067f050622f00a7ea745cd749
Colin <colinraminoson@gmail.com>
2023-02-08 11:05:18 +0300

    model

--- application/models/Model.php
+++ application/models/Model.php
@@ -3,19 +3,45 @@
 
     class Model extends CI_Model 
     {
+        public function GetUser($mail,$pass)
+        {
+            $sql = "select * from Utilisateur ";
+            $query = $this->db->query($sql);
+            $result = $query->result_array();
+            return $result;
+
+        }
        
         public function checkLogin($mail,$pass)
         {
+            $listeUser=$this->Model->GetUser();
             $valiny = false;
-            if($mail == 'azerty@gmail.com' && $pass == 'ok')
-            {
-                $valiny = true;
-            }
+             for ($i=0;$i<count($listeUser);$i++){
+                if($mail == $listeUser[$i] ['email'] && $pass == $listeUser[$i] ['mdp'])
+                {
+                    $valiny = true;
+                }
+             }
+           
             return $valiny;
         }
+       
         public function listeObjet()
         {   
             $sql = "select * from Objects";
+            $query = $this->db->query($sql);
+            $result = array();
+
+            foreach($query->result_array() as $row)
+            {
+            $result[] = $row;
+            }
+            return $result;
+        }
+        public function listeObjetById($id)
+        {   
+            $sql = "select * from Objects where idObjet='%s'";
+            $sql = sprintf($sql,$id);
             $query = $this->db->query($sql);
             $result = array();
 
