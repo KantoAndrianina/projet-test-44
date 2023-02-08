d756078b1647f6687a7dfb3e94bf1cae693d03c1
Colin <colinraminoson@gmail.com>
2023-02-08 09:26:55 +0300

    content

--- application/views/page/content.php
+++ application/views/page/content.php
@@ -1,97 +1,20 @@
     <div class="container-fluid" data-aos="fade" data-aos-delay="500">
       <div class="row">
-        <div class="col-lg-4">
-
-          <div class="image-wrap-2">
-            <div class="image-info">
-              <h2 class="mb-3">Nature</h2>
-              <a href="single.html" class="btn btn-outline-white py-2 px-4">More Photos</a>
-            </div>
-            <img src="<?php echo base_url();?>assets/css-home/images/img_1.jpg" alt="Image" class="img-fluid">
-          </div>
-
-        </div>
+      <?php if(isset($listeObjet))
+        {
+        foreach ($listeObjet as $objet) { ?>
         <div class="col-lg-4">
           <div class="image-wrap-2">
             <div class="image-info">
-              <h2 class="mb-3">Portrait</h2>
+              <h2 class="mb-3"><?php echo $objet['NomObjet']?></h2>
               <a href="single.html" class="btn btn-outline-white py-2 px-4">More Photos</a>
             </div>
-            <img src="<?php echo base_url();?>assets/css-home/images/img_2.jpg" alt="Image" class="img-fluid">
+            <img src="<?php echo base_url();?>assets/css-home/images/<?php echo $objet['imgMain']?>" alt="Image" class="img-fluid">
           </div>
         </div>
-        <div class="col-lg-4">
-          <div class="image-wrap-2">
-            <div class="image-info">
-              <h2 class="mb-3">People</h2>
-              <a href="single.html" class="btn btn-outline-white py-2 px-4">More Photos</a>
-            </div>
-            <img src="<?php echo base_url();?>assets/css-home/images/img_3.jpg" alt="Image" class="img-fluid">
-          </div>
-        </div>
-
-        <div class="col-lg-4">
-          <div class="image-wrap-2">
-            <div class="image-info">
-              <h2 class="mb-3">Architecture</h2>
-              <a href="single.html" class="btn btn-outline-white py-2 px-4">More Photos</a>
-            </div>
-            <img src="<?php echo base_url();?>assets/css-home/images/img_4.jpg" alt="Image" class="img-fluid">
-          </div>
-        </div>
-
-        <div class="col-lg-4">
-          <div class="image-wrap-2">
-            <div class="image-info">
-              <h2 class="mb-3">Animals</h2>
-              <a href="single.html" class="btn btn-outline-white py-2 px-4">More Photos</a>
-            </div>
-            <img src="<?php echo base_url();?>assets/css-home/images/img_5.jpg" alt="Image" class="img-fluid">
-          </div>
-        </div>
-
-        <div class="col-lg-4">
-          <div class="image-wrap-2">
-            <div class="image-info">
-              <h2 class="mb-3">Sports</h2>
-              <a href="single.html" class="btn btn-outline-white py-2 px-4">More Photos</a>
-            </div>
-            <img src="<?php echo base_url();?>assets/css-home/images/img_6.jpg" alt="Image" class="img-fluid">
-          </div>
-        </div>
-
-        <div class="col-lg-4">
-          <div class="image-wrap-2">
-            <div class="image-info">
-              <h2 class="mb-3">Travel</h2>
-              <a href="single.html" class="btn btn-outline-white py-2 px-4">More Photos</a>
-            </div>
-            <img src="<?php echo base_url();?>assets/css-home/images/img_7.jpg" alt="Image" class="img-fluid">
-          </div>
-        </div>
-
-        <div class="col-lg-4">
-          <div class="image-wrap-2">
-            <div class="image-info">
-              <h2 class="mb-3">People</h2>
-              <a href="single.html" class="btn btn-outline-white py-2 px-4">More Photos</a>
-            </div>
-            <img src="<?php echo base_url();?>assets/css-home/images/img_3.jpg" alt="Image" class="img-fluid">
-          </div>
-        </div>
-
-        <div class="col-lg-4">
-          <div class="image-wrap-2">
-            <div class="image-info">
-              <h2 class="mb-3">Architecture</h2>
-              <a href="single.html" class="btn btn-outline-white py-2 px-4">More Photos</a>
-            </div>
-            <img src="<?php echo base_url();?>assets/css-home/images/img_4.jpg" alt="Image" class="img-fluid">
-          </div>
-        </div>
-
+        <?php }}?>
       </div>
-    </div>
+    </div> 
 
     <div class="footer py-4">
       <div class="container-fluid text-center">
